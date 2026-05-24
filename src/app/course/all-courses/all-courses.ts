4cb1938212bcc9a76cba0675e6a7c227b096cc2f
fadyfadd <fady_faddoul@hotmail.com>
2026-05-24 18:30:55 +0300

    assigning paginator

--- src/app/course/all-courses/all-courses.ts
+++ src/app/course/all-courses/all-courses.ts
@@ -7,11 +7,14 @@ import { BehaviorSubject } from 'rxjs';
 import { CourseDto } from '../dtos';
 import { CommonModule } from '@angular/common';
 import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-all-courses',
-  imports: [MatTableModule, CommonModule, MatSortModule],
+  imports: [MatTableModule, CommonModule, MatSortModule, MatPaginator , MatButtonModule, MatIconModule],
   templateUrl: './all-courses.html',
   styleUrl: './all-courses.css',
 })
@@ -21,16 +24,21 @@ export class AllCourses implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort!: MatSort;
   dataSource = new MatTableDataSource<CourseDto>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+
   }
 
   private http: HttpClient = inject(HttpClient);
   private configService: any = inject(ConfigService);
   public courses$: BehaviorSubject<CourseDto[]> = new BehaviorSubject<CourseDto[]>([]);
 
-  public displayedColumns: string[] = ['id', 'title', 'categoryId', 'categoryName' , 'actions'];
+  public displayedColumns: string[] = ['id', 'title', 'categoryId', 'categoryName', 'actions'];
 
   ngOnInit(): void {
 
